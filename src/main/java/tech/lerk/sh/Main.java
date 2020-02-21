8e8e13657cf1b7079547b6280adc06846a6ed65d
Lukas Fülling <lukas@k40s.net>
2020-02-21 05:27:32 +0100

    use yahs, style more pages, version bump

--- src/main/java/tech/lerk/sh/Main.java
+++ src/main/java/tech/lerk/sh/Main.java
@@ -1,11 +1,14 @@
 package tech.lerk.sh;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sh.lrk.yahs.*;
 
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.HashMap;
 
 /**
  * Main class.
@@ -38,16 +41,42 @@ public class Main {
 
         log.info("Generating routes...");
         Routes routes = new Routes();
-        routes.add("GET", "/", "html/landing.html");
-        routes.add("GET", "/style.css", "html/style.css");
-        routes.add("GET", "/lib.js", "html/lib.js");
-        routes.add("GET", "/favicon.ico", "images/favicon.ico");
-        routes.add("GET", "/background.jpg", "images/background.jpg");
-        routes.add("POST", "/", new ResolverResponse(configManager, databaseManager));
+        routes.add(Method.GET, "/", "html/landing.html");
+        routes.add(Method.GET, "/style.css", "css/style.css");
+        routes.add(Method.GET, "/lib.js", "js/lib.js");
+        routes.add(Method.GET, "/favicon.ico", "images/favicon.ico");
+        routes.add(Method.GET, "/background.jpg", "images/background.jpg");
+        routes.add(Method.POST, "/", new ResolverResponse(configManager, databaseManager));
+        routes.addCatchAll(req -> {
+            String key = req.getUrl().replaceFirst("/", "");
+            try {
+                String urlForKey = databaseManager.getUrlForKey(key);
+                if (urlForKey != null && !urlForKey.isEmpty()) {
+                    String message = "You are being redirected...";
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Location", urlForKey);
+                    return new Response(message.getBytes(), Status.TEMPORARY_REDIRECT,
+                            ContentType.TEXT_PLAIN, headers);
+                }
+            } catch (DatabaseManager.NoResultException e) {
+                log.info("No value found for key: '" + key + "'!");
+            } catch (SQLException e) {
+                log.error("Unable to resolve key: '" + key + "'!", e);
+            }
+            try {
+                Handlebars handlebars = new Handlebars();
+                Template template = handlebars.compile("html/error");
+                return new Response(template.apply("Not Found!"),
+                        Status.NOT_FOUND, ContentType.TEXT_HTML);
+            } catch (IOException e) {
+                log.error("Unable to render error!");
+                return new Response("Not Found!",
+                        Status.NOT_FOUND, ContentType.TEXT_HTML);
+            }
+        });
 
         log.info("Starting Server...");
 
-        ServerThread server = new ServerThread(routes, configManager, databaseManager);
-        new Thread(server, "ServerThread").start();
+        Server.start(routes, configManager.getPort(), configManager.getMaxSize());
     }
 }
--- src/main/java/tech/lerk/sh/ResolverResponse.java
+++ src/main/java/tech/lerk/sh/ResolverResponse.java
@@ -1,8 +1,12 @@
 package tech.lerk.sh;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sh.lrk.yahs.*;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -26,10 +30,17 @@ public class ResolverResponse implements IResponse {
 
     private final ConfigManager configManager;
     private final DatabaseManager databaseManager;
+    private final Handlebars handlebars = new Handlebars();
+    private Template errorTemplate;
 
     ResolverResponse(ConfigManager configManager, DatabaseManager databaseManager) {
         this.configManager = configManager;
         this.databaseManager = databaseManager;
+        try {
+            errorTemplate = handlebars.compile("html/error");
+        } catch (IOException e) {
+            log.error("Error compiling error template!", e);
+        }
     }
 
     @Override
@@ -43,22 +54,23 @@ public class ResolverResponse implements IResponse {
             }
 
             if (value.isEmpty()) {
-                return new Response("URL may not be empty!", Response.BAD_REQUEST);
+                return new Response(applyErrorTemplate("URL may not be empty!"),
+                        Status.BAD_REQUEST, ContentType.TEXT_HTML);
             }
 
             try {
                 value = URLDecoder.decode(value, StandardCharsets.UTF_8.name());
             } catch (UnsupportedEncodingException e) {
                 log.warn("Unable to decode url: '" + value + "'!", e);
-                return new Response("Unable to parse url '" + value + "':\n\t" + e.getMessage(),
-                        Response.BAD_REQUEST);
+                return new Response(applyErrorTemplate("Unable to parse url '" + value + "':\n\t" + e.getMessage()),
+                        Status.BAD_REQUEST, ContentType.TEXT_HTML);
             }
 
             try {
                 new URL(value);
             } catch (MalformedURLException e) {
-                return new Response("URL '" + value + "' is invalid!\n" + e.getMessage(),
-                        Response.BAD_REQUEST);
+                return new Response(applyErrorTemplate("Invalid URL!"),
+                        Status.BAD_REQUEST, ContentType.TEXT_HTML);
             }
 
             try {
@@ -67,23 +79,40 @@ public class ResolverResponse implements IResponse {
                 log.error("Unable to save url: '" + value + "' with key: '" + key + "'!", e);
                 if (e.getMessage().contains(DUPLICATE_URL_ERROR_MSG)) {
                     try {
-                        return new Response("This URL is already saved as: '" + databaseManager.getKeyForUrl(value) + "'");
+                        Template template = handlebars.compile("html/already-existing");
+                        return new Response(template.apply(databaseManager.getKeyForUrl(value)), ContentType.TEXT_HTML);
                     } catch (SQLException ex) {
                         log.error("Unable to look up key for url: '" + value + "' but it seems to be existing!", ex);
-                        return new Response("Unable to save url:\n\t" + e.getMessage() +
-                                "\nError looking up already existing url!\n\t" + ex.getMessage(),
-                                Response.INTERNAL_SERVER_ERROR);
+                        return new Response(applyErrorTemplate("Unable to save url:\n\t" + e.getMessage() +
+                                "\n\nError looking up already existing url!\n\t" + ex.getMessage()),
+                                Status.INTERNAL_SERVER_ERROR, ContentType.TEXT_HTML);
+                    } catch (IOException ex) {
+                        log.error("Unable to render response!");
+                        return new Response(applyErrorTemplate("Unable to save url:\n\t" + e.getMessage() +
+                                "\n\nError looking up already existing url!\n\t" + ex.getMessage()),
+                                Status.INTERNAL_SERVER_ERROR, ContentType.TEXT_HTML);
                     }
                 } else {
-                    return new Response("Unable to save url:\n\t" + e.getMessage(),
-                            Response.INTERNAL_SERVER_ERROR);
+                    return new Response(applyErrorTemplate("Unable to save url:\n\t" + e.getMessage()),
+                            Status.INTERNAL_SERVER_ERROR, ContentType.TEXT_HTML);
                 }
             }
 
             return new Response("OK!\nURL '" + value + "' was saved as '" + key + "'!");
         } else {
-            return new Response("You need to enter the correct password for this to work!",
-                    Response.UNAUTHORIZED);
+            return new Response(applyErrorTemplate("You entered an incorrect password!"),
+                    Status.UNAUTHORIZED, ContentType.TEXT_HTML);
         }
+    }
+
+    private String applyErrorTemplate(String message) {
+        if (errorTemplate != null) {
+            try {
+                return errorTemplate.apply(message);
+            } catch (IOException e) {
+                log.error("Error applying template!", e);
+            }
+        }
+        return message;
     }
 }
